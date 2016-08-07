0a386e12359b0b74314aa9ad444fb4b5027304c4
GFV450 <gfv450@gmail.com>
2016-08-06 18:05:39 -0700

    Added Friend Challenges functionality

---
+++ iChallenge/Main/Friend Challenges/MainFriendChallengesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MainFriendChallengesCollectionViewCell.swift
+//  iChallenge
+//
+//  Created by Gian Vitola on 8/6/16.
+//  Copyright © 2016 Gian Franco Vitola. All rights reserved.
+//
+
+import UIKit
+
+class MainFriendChallengesCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var challengerImage: UIImageView!
+    @IBOutlet weak var challengeName: UILabel!
+    
+    var challengeDescription: String?
+    var challenge: Challenge?
+}
--- iChallenge/Main/Friend Challenges/MainFriendChallengesViewController.swift
+++ iChallenge/Main/Friend Challenges/MainFriendChallengesViewController.swift
@@ -10,5 +10,73 @@ import UIKit
 
 class MainFriendChallengesViewController: UIViewController
 {
+    var friendChallengeArray = [Challenge]()
     
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var friendChallengesCollectionView: UICollectionView!
+    
+    
+    
+    // MARK: - View Lifecycles
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        friendChallengeArray.removeAll()
+        self.retrieveFriendChallenges()
+    }
+}
+
+extension MainFriendChallengesViewController: UICollectionViewDelegate, UICollectionViewDataSource
+{
+    func retrieveFriendChallenges()
+    {
+        FirebaseHelper.queryFriendChallenges({ (challenge: Challenge) -> Void in
+            //Appends the user retrieved from the Database on userArray
+            self.friendChallengeArray.append(challenge)
+            
+            self.friendChallengesCollectionView.reloadData()
+        })
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return friendChallengeArray.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = friendChallengesCollectionView.dequeueReusableCellWithReuseIdentifier("MainFriendChallengesCollectionViewCell", forIndexPath: indexPath) as! MainFriendChallengesCollectionViewCell
+        
+        let challenge = friendChallengeArray[indexPath.row]
+        
+        let profileImageNSURL = NSURL(string: challenge.challengerProfileImage)
+        cell.challengerImage.af_setImageWithURL(profileImageNSURL!)
+        cell.challengerImage.image? = (cell.challengerImage.image?.af_imageRoundedIntoCircle())!
+        
+        cell.challengeName.text = challenge.challengeTitle
+        
+        cell.challenge = challenge
+        
+        return cell
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        if segue.identifier == "ChallengeSegue"
+        {
+            let cell = sender as! MainFriendChallengesCollectionViewCell
+            let challengeController = segue.destinationViewController as! ChallengeViewController
+            
+            challengeController.challenge = cell.challenge
+        }
+    }
 }
